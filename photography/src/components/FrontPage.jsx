dd16953a66141c15f336134333c8058f81476b38
Alketa <alketa_1996@hotmail.com>
2021-02-03 20:53:10 +0200

    initializing the webpage

---
+++ photography/src/components/FrontPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class frontPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>it workssss</h1>
+      </div>
+    );
+  }
+}
+
+export default frontPage;
