4dcbaf862558ca276b50cd3bfb83af6260698db5
Alketa <alketa_1996@hotmail.com>
2021-02-07 02:39:59 +0200

    styling the contact form

--- photo/src/components/Contact.js
+++ photo/src/components/Contact.js
@@ -16,14 +16,19 @@ export default function Contact() {
 
     return (
         <form className="contact-form" onSubmit={sendEmail}>
-            <input type="hidden" name="contact_number" />
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
+            <div className="style-form">
+                <input type="hidden" name="contact_number" />
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+            </div>
+            <div>
+                <img src="./img/contact.jpeg" alt="koira" width="450px" height="620px"></img>
+            </div>
         </form>
     );
 }
