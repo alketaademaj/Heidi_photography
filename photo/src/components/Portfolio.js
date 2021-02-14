77b6af9b71557763b47a49b6948616c53e578dfb
Alketa <alketa_1996@hotmail.com>
2021-02-14 23:22:31 +0200

    initializing the path

--- photo/src/components/Portfolio.js
+++ photo/src/components/Portfolio.js
@@ -9,12 +9,18 @@ export class Portfolio extends Component {
 
         }
     }
+
+    PictureOnePortfolio = () => {
+        //create here the pathway on click to go to the pictureOne js file
+
+    }
+
     render() {
         return (
             <div>
                 <div className="picturebox">
                     <div className="singlebox">
-                        <button className="view-button">View</button>
+                        <button className="view-button" onClick={ } > View </button>
                     </div>
                     <div className="singlebox">
                         <button className="view-button">View</button>
